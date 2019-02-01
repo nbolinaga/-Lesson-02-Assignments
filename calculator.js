485ab25d95f53b8c6ebbf7c187b1c21305510af5
nbolinaga <nbolinaga17@gmail.com>
2019-01-31 19:22:58 -0800

    Fixed Syntax Error

--- calculator.js
+++ calculator.js
@@ -1,10 +1,10 @@
 // CALCULATOR Lesson 02 Assignment 1
 
 // VARIABLES
-let number_1 = (12);
-let number_2 = (45);
-let number_3 = (3);
-let number_4 = (76);
+let number_1 = 12;
+let number_2 = 45;
+let number_3 = 3;
+let number_4 = 76;
 
 // SHOW VARIABLES ON CONSOLE
 console.log('n1=' + number_1, 'n2=' + number_2, 'n3=' + number_3, 'n4=' + number_4);
